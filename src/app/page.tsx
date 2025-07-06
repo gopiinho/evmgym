dc19a5db3b771da3f6eec4a11ca3ca7d29954dbc
Gurpreet <gs109690@gmail.com>
2025-07-06 23:04:47 +0530

    chore: set up prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <h1 className="text-4xl font-semibold">EVM Gym</h1>
     </div>
   );
